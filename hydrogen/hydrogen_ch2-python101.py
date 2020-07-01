70cc1c0d37b43943c7e85f74bd6fd6c0901f1c7a
Elliott Whitling <elliottw@gointerject.com>
2020-06-30 21:35:34 -0700

    Added Python notebook.

--- hydrogen/hydrogen_ch2-python101.py
+++ hydrogen/hydrogen_ch2-python101.py
@@ -15,15 +15,47 @@ os.listdir()
 
 # %% codecell
 # data types
+my_number = 5.0
+my_string = "octopus"
+my_boolean = False
 
 # %% codecell
-# conditionals
+# data structures
+# list
+my_list = ["octopus", "squid", "cuttlefish"]
+my_list[0]
+my_list
+
+# tuple
+
+# set
+
+
+# dictionary
+my_dict = dict()
+my_dict[''] = 1
+my_dict[''] = 2
+
+# dataframes
+
+# %% codecell
+# conditionals to control a logical flow of work
+if my_boolean:
+	print("It was true!")
+else:
+	print("False!")
+
+if my_boolean == True:
+	print("This is the same as just using 'if my_boolean:'")
+
+print("Does the string equal the boolean? %s" % (my_string == my_boolean))
 
 # %% codecell
 # loops
 
 # %% codecell
 # asserts
+assert  my_boolean==False, "Asserts check assumptions."
 
 # %% codecell
 # create a function
@@ -40,3 +72,28 @@ def is_it_an_octopus(limbs, thing):
         print("This octopus is well disguised as a %s!" % thing)
 
 is_it_an_octopus(8, "dog")
+is_it_an_octopus(4, "cat")
+is_it_an_octopus(9, "Gorgon!")
+
+# %% codecell
+# recursive functions
+def walk_directory(path):
+	contents = os.listdir(path)
+
+	for item in contents:
+		new_path = os.path.join(path, item)
+
+		if os.path.isdir(new_path):
+			walk_directory(new_path)
+		elif os.path.isfile(new_path):
+			print("We found a file: %s" % new_path)
+		else:
+			print("What the heck is this! %s " % new_path)
+
+walk_directory("./")
+
+
+
+
+# %% codecell
+# list comprehensions and generators
